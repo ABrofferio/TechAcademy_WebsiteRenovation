139db387e24eb74af98cedcab7384775755f6670
makaliifernandez <makaliifernandez@gmail.com>
2016-06-15 14:27:55 -0700

    All js files are now clean and free from repetition. Every page now loads main.js which contains all of the common code that was being duplicated across several js files. Some pages also contains an additional page specific js file which contains code unique to that page. Also cleaned up many errors along the way. Also fixed the images in the testimonials page so that it scales properly based on the browser's width

--- js/index.js
+++ js/index.js
@@ -1,21 +1,17 @@
 $(document).ready(function () {
 
-    // EVENT HANDLERS
 
-    $('.clickForContactForm').on('click', function(){
-        console.log('click for contact form');
-        waitThenDoSomething(0, 'displayPopup');
-    });
-
-    $('.slideanim').hover(
-        function () { $(this).addClass('fadeIn') },
-        function () { $(this).removeClass('fadeIn') });
-
-
-    // AUTO FUNCTIONS
+    /**************************************************************************
+     * 
+     *************************************************************************/
 
     waitThenDoSomething(60000, "displayPopup"); //this function waits 60000milliseconds before displaying a popup.
 
+
+    /**************************************************************************
+     * Testimonials Slider. Used only on this page.
+     *************************************************************************/
+    
     $(function () {
         var LoopIntervalsInMilliseconds = 2000;
 
@@ -62,39 +58,9 @@ $(document).ready(function () {
     });
 
 
-    function waitThenDoSomething(waitTimeInMS, myAction) {
-        setTimeout(function () {
-            if (myAction == "displayPopup") {
-                $('#myModalPopUpContact').modal('show');
-            }
-        }, waitTimeInMS); //time in ms to wait before modal dialog box shows up.
-    }
-
-    $("a").click(function () { window.onbeforeunload = dontGiveMePopup; }); //don't make popup appear just because user clicked our site's hyperlink.
-    document.getElementById("yes-modal-btn").onclick = function () { showContactForm(0,"displayPopup") };//this shows the contact form when yes is clicked on first popUp
-    $("header").hide();
-    $(".text-center").scrollTop(0);
-    $(".container").hide();
-    $("#coverPic").fadeOut(2000);
-    $(".container").delay(2000);
-    $("header").delay(2000);
-    $(".container").fadeIn(200);
-    $("header").fadeIn(200);
-    $(".text-center").scrollTop(0);
-    $("navText").click(function () { $(this).removeClass("navText")});
-    $(window).ready(function () { $(window).scroll() });
-    $(window).scroll(function () {
-        $(".slideanim").each(function () {
-            var pos = $(this).offset().top;
-
-            var winTop = $(window).scrollTop();
-
-            if (pos < winTop + $(window).height()) {
-                $(this).addClass("slide");
-            }
-        });
-    });
-
+    /**************************************************************************
+     *
+     *************************************************************************/
 
 
 });
--- js/main.js
+++ js/main.js
@@ -1,4 +1,68 @@
 /**
- * Created by mamf8 on 6/15/2016.
+ * Created by Makali'i on 6/15/2016.
+ * This file should be loaded in all html pages.
+ * // TODO: 6.15.16 Makali'i - Everything in here is accessible globally. Place into modules once files are DRY and structure established.
  */
+
+/**************************************************************************
+ *
+ *************************************************************************/
+
+function waitThenDoSomething(waitTimeInMS, myAction) {
+    setTimeout(function () {
+        if (myAction == "displayPopup") {
+            $('#myModalPopUpContact').modal('show');
+        }
+    }, waitTimeInMS); //time in ms to wait before modal dialog box shows up.
+}
+
+
+
+/**************************************************************************
+ *  USEFUL CLASSES WITH EVENTS
+ *************************************************************************/
+$('.clickForContactForm').on('click', function(){
+    console.log('click for contact form');
+    waitThenDoSomething(0, 'displayPopup');
+});
+
+$('.slideanim').hover(
+    function () { $(this).addClass('fadeIn') },
+    function () { $(this).removeClass('fadeIn') });
+
+
+
+/**************************************************************************
+ * Reveals/fades in/slides up headers, containers, and text etc...
+ *************************************************************************/
+
+$(document).ready(function () {
+    $("header").hide();
+    $(".text-center").scrollTop(0);
+    $(".container").hide();
+    $("#coverPic").fadeOut(2000);
+    $(".container").fadeIn(4000);
+    $("header").fadeIn(4000);
+    $(".text-center").scrollTop(0);
+    $("navText").click(function () { $(this).removeClass("navText")})
+    $(window).ready(function () { $(window).scroll() });
+    $(window).scroll(function () {
+        $(".slideanim").each(function () {
+            var pos = $(this).offset().top;
+
+            var winTop = $(window).scrollTop();
+
+            if (pos < winTop + $(window).height()) {
+                $(this).addClass("slide");
+            }
+        });
+    });
+});
+
+
+
+
+
+
+
 
